ac926308f99585bcb6afdcc991774086b0ceecbe
JusterZhu <zhuzhen723723@163.com>
2021-08-21 23:24:50 +0800

    添加组件页面

--- src/Juster.SpaceStation/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ src/Juster.SpaceStation/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,71 +13,85 @@ namespace Juster.SpaceStation
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 1 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 2 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 3 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 4 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 5 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 6 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 7 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 8 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 9 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Juster.SpaceStation;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "F:\博客相关资料\blazor\Juster.SpaceStation\_Imports.razor"
+#line 10 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
 using Juster.SpaceStation.Shared;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
+using BootstrapBlazor.Components;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "F:\博客相关资料\blazor\JusterSpaceStation\src\Juster.SpaceStation\_Imports.razor"
+using BootstrapBlazor.Localization;
 
 #line default
 #line hidden
