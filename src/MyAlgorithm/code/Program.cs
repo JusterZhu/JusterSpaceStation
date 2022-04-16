55d72e0264f689001e97ff3945895a301d91f758
Juster.zhu <zhuzhen723723@163.com>
2022-04-16 23:53:49 +0800

    添加tree

--- src/MyAlgorithm/code/Program.cs
+++ src/MyAlgorithm/code/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAlgorithm.Tree;
+using System;
 
 namespace MyAlgorithm
 {
@@ -6,20 +7,33 @@ namespace MyAlgorithm
     {
         static void Main(string[] args)
         {
-            int result = Factorial(4);
-            Console.WriteLine(result);
-        }
+            //创建树
+            var tree = new BinaryTree();
 
-        public static int Factorial(int n)
-        {
-            if (n == 1)
-            {
-                return 1;
-            }
-            else
-            {
-                return Factorial(n - 1) * n;
-            }
+            //创建需要的节点
+            TreeNode root = new TreeNode(1,"名册");
+            TreeNode node2 = new TreeNode(2, "张三");
+            TreeNode node3 = new TreeNode(3,"李四");
+            TreeNode node4 = new TreeNode(4,"王五");
+
+
+            //手动创建二叉树
+            root.Left = node2;
+            root.Right = node3;
+            node3.Right = node4;
+            tree.SetRoot(root);
+
+            //测试
+            Console.WriteLine("前序");
+            tree.PreOrder();
+
+            Console.WriteLine("中序");
+            tree.InfixOrder();
+
+            Console.WriteLine("后序");
+            tree.PostOrder();
+
+            Console.Read();
         }
     }
 }
---
+++ src/MyAlgorithm/code/Tree/BinaryTree.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAlgorithm.Tree
+{
+    public class TreeNode 
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public TreeNode Left { get; set; }
+
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int id,string name) 
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"TreeNode [id = { Id } , name ={ Name }]";
+        }
+
+        /// <summary>
+        /// 前序遍历
+        /// </summary>
+        public void PreOrder()
+        {
+            Console.WriteLine(this);
+            //递归向左子树前序遍历
+            if (this.Left != null) 
+            {
+                this.Left.PreOrder();
+            }
+            //递归向右子树前序遍历
+            if (this.Right != null)
+            {
+                this.Right.PreOrder();
+            }
+        }
+
+        /// <summary>
+        /// 中序遍历
+        /// </summary>
+        public void InfixOrder() 
+        {
+            //递归向左子树中序遍历
+            if (this.Left != null)
+            {
+                this.Left.InfixOrder();
+            }
+
+            //输出父节点
+            Console.WriteLine(this);
+
+            //递归向右子树中序遍历
+
+            if (this.Right != null)
+            {
+                this.Right.InfixOrder();
+            }
+        }
+
+        /// <summary>
+        /// 后续遍历
+        /// </summary>
+        public void PostOrder() 
+        {
+            if (this.Left != null)
+            {
+                this.Left.PreOrder();
+            }
+
+            if (this.Right != null)
+            {
+                this.Right.PreOrder();
+            }
+
+            Console.WriteLine(this);
+        }
+    }
+
+    public class BinaryTree
+    {
+        private TreeNode root;
+
+        public void SetRoot(TreeNode root) 
+        {
+            this.root = root;
+        }
+
+        //前序遍历
+        public void PreOrder() 
+        {
+            if (this.root != null)
+            {
+                this.root.PreOrder();
+            }
+            else 
+            {
+                Console.WriteLine("二叉树为空，无法遍历！");
+            }
+        }
+
+        //中序遍历
+        public void InfixOrder() 
+        {
+            if (this.root != null)
+            {
+                this.root.InfixOrder();
+            }
+            else
+            {
+                Console.WriteLine("二叉树为空，无法遍历！");
+            }
+        }
+
+        //后续遍历
+        public void PostOrder() 
+        {
+            if (this.root != null)
+            {
+                this.root.PostOrder();
+            }
+            else
+            {
+                Console.WriteLine("二叉树为空，无法遍历！");
+            }
+        }
+    }
+}
