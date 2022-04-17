2450269ff9d614e474fad43b6569f5d3fc057bfc
Juster.zhu <zhuzhen723723@163.com>
2022-04-17 21:17:20 +0800

    Tree

--- src/MyAlgorithm/code/Program.cs
+++ src/MyAlgorithm/code/Program.cs
@@ -7,32 +7,35 @@ namespace MyAlgorithm
     {
         static void Main(string[] args)
         {
-            //创建树
-            var tree = new BinaryTree();
+            ////创建树
+            //var tree = new BinaryTree();
 
-            //创建需要的节点
-            TreeNode root = new TreeNode(1,"名册");
-            TreeNode node2 = new TreeNode(2, "张三");
-            TreeNode node3 = new TreeNode(3,"李四");
-            TreeNode node4 = new TreeNode(4,"王五");
+            ////创建需要的节点
+            //TreeNode root = new TreeNode(1,"名册");
+            //TreeNode node2 = new TreeNode(2, "张三");
+            //TreeNode node3 = new TreeNode(3,"李四");
+            //TreeNode node4 = new TreeNode(4,"王五");
+            //TreeNode node5 = new TreeNode(5, "赵六");
 
 
-            //手动创建二叉树
-            root.Left = node2;
-            root.Right = node3;
-            node3.Right = node4;
-            tree.SetRoot(root);
+            ////手动创建二叉树
+            //root.Left = node2;
+            //root.Right = node3;
+            //node3.Left = node5;
+            //node3.Right = node4;
+            //tree.SetRoot(root);
 
-            //测试
-            Console.WriteLine("前序");
-            tree.PreOrder();
-
-            Console.WriteLine("中序");
-            tree.InfixOrder();
-
-            Console.WriteLine("后序");
-            tree.PostOrder();
-
+            //Console.WriteLine("删除前，前序遍历");
+            //tree.PreOrder();
+            //tree.DeleteNode(5);
+            //Console.WriteLine("删除后，前序遍历");
+            //tree.PreOrder();
+            //tree.DeleteNode(3);
+            //Console.WriteLine("删除后，前序遍历");
+            //tree.PreOrder();
+            int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(array);
+            arrayBinaryTree.PreOrder();
             Console.Read();
         }
     }
---
+++ src/MyAlgorithm/code/Tree/ArrayBinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAlgorithm.Tree
+{
+
+    internal class ArrayBinaryTree
+    {
+        private int[] array;
+
+        public ArrayBinaryTree(int[] array)
+        {
+           this.array = array;
+        }
+
+        public void PreOrder()
+        {
+            //如果数组为空，或者array.length = 0
+            if (array == null || array.Length == 0)
+            {
+                Console.WriteLine("数组为空，不能按照二叉树的前序遍历");
+            }
+            //输出当前这个元素
+            Console.WriteLine(array[0]);
+            //向左递归遍历
+            if (2 * 0 + 1 < array.Length)
+            {
+                PreOrder(2 * 0 + 1);
+            }
+            //向右递归遍历
+            if (2 * 0 + 2 < array.Length)
+            {
+                PreOrder(2 * 0 + 2);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index">数组下标</param>
+        public void PreOrder(int index)
+        {
+            //如果数组为空，或者array.length = 0
+            if (array == null || array.Length == 0)
+            {
+                Console.WriteLine("数组为空，不能按照二叉树的前序遍历");
+            }
+            //输出当前这个元素
+            Console.WriteLine(array[index]);
+            //向左递归遍历
+            if (2 * index + 1 < array.Length)
+            {
+                PreOrder(2 * index + 1);
+            }
+            //向右递归遍历
+            if (2 * index + 2 < array.Length)
+            {
+                PreOrder(2 * index + 2);
+            }
+        }
+    }
+}
