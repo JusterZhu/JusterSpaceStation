b98ddc32f081a886d298c7f15fb347af12a610ea
Juster.zhu <zhuzhen723723@163.com>
2022-05-09 00:18:57 +0800

    堆排序

--- src/MyAlgorithm/code/Program.cs
+++ src/MyAlgorithm/code/Program.cs
@@ -8,45 +8,9 @@ namespace MyAlgorithm
     {
         static void Main(string[] args)
         {
-            //顶点个数
-            int n = 8;
-            //string[] vertexs = { "A", "B", "C", "D", "E" };
-            string[] vertexs = { "1", "2", "3", "4", "5", "6", "7", "8" };
-            //创建图对象
-            MyGraph graph = new MyGraph(n);
-            //添加顶点
-            foreach (var item in vertexs)
-            {
-                graph.InsertVertex(item);
-            }
-            //添加边 A-B A-C B-C B-D B-E
-            //graph.InsertEdge(0, 1, 1);//A-B
-            //graph.InsertEdge(0, 2, 1);
-            //graph.InsertEdge(1, 2, 1);
-            //graph.InsertEdge(1, 3, 1);
-            //graph.InsertEdge(1, 4, 1);
-
-            graph.InsertEdge(0, 1, 1);
-            graph.InsertEdge(0, 2, 1);
-            graph.InsertEdge(1, 3, 1);
-            graph.InsertEdge(1, 4, 1);
-            graph.InsertEdge(3, 7, 1);
-            graph.InsertEdge(4, 7, 1);
-            graph.InsertEdge(2, 5, 1);
-            graph.InsertEdge(2, 6, 1);
-            graph.InsertEdge(5, 6, 1);
-
-            //打印矩阵
-            graph.PrintGraph();
-
-            //DFS遍历
-            //Console.WriteLine("深度遍历");
-            //graph.DFS();
-
-            //BFS遍历
-            Console.WriteLine("广度遍历");
-            graph.BFS();
-
+            //数组进行升序排序
+            int[] arr = { 4, 6, 8, 5, 9,-1,90,89,56,-999 };
+            HeapSort.HeapSort0(arr);
             Console.Read();
         }
     }
---
+++ src/MyAlgorithm/code/Tree/HeapSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAlgorithm.Tree
+{
+    public class HeapSort
+    {
+        public static void HeapSort0(int[] arr)
+        {
+            int temp = 0;
+            Console.WriteLine("堆排序！");
+            //分步完成
+            //AddJustHeap(arr, 1, arr.Length);
+            //Console.WriteLine("第一次" + string.Join(' ',arr));//4,9,8,5,6
+
+            //AddJustHeap(arr, 0, arr.Length);
+            //Console.WriteLine("第二次" + string.Join(' ', arr));//9,6,8,5,4
+
+            for (int i = arr.Length / 2 - 1; i >= 0; i--)
+            {
+                AddJustHeap(arr, i, arr.Length);
+            }
+            //Console.WriteLine("第三次" + string.Join(' ', arr));
+
+            for (int j = arr.Length - 1; j > 0; j--)
+            {
+                temp = arr[j];
+                arr[j] = arr[0];
+                arr[0] = temp;
+                AddJustHeap(arr, 0, j);
+            }
+            Console.WriteLine("数组=" + string.Join(' ', arr));
+        }
+
+        /// <summary>
+        /// 完成将以i指向的对应的非叶子节点的树调整为一个大顶堆
+        /// 将一个数组（二叉树），调整为一个大顶堆
+        /// 举例int [] arr = {4,6,8,5,9}; =>i = 1 => addjustheap => 得到{ 4，9，8，5，6 }
+        /// 如果我们再次调用addjustheap 传入的是i = 0 =>{4,9,8,5,6}=>{9,6,8,5,4}
+        /// </summary>
+        /// <param name="arr">待调整数组</param>
+        /// <param name="i">表示非叶子节点在数组中索引</param>
+        /// <param name="length">表示堆多少个元素进行继续调整，length在逐渐减少</param>
+        public static void AddJustHeap(int[] arr, int i, int length)
+        {
+            int temp = arr[i];//先取数当前元素的值，保存在临时变量
+            //开始调整
+            //1. k = i * 2 + 1 , k 是i节点的左子节点
+            for (int k = i * 2 + 1; k < length; k = k * 2 +1)
+            {
+                //说明左子节点的值小于又子节点的值
+                if (k + 1 < length && arr[k] < arr[k+1])
+                {
+                    k++; // k指向又子节点
+                }
+
+                //如果子节点大于父节点
+                if (arr[k] > temp)
+                {
+                    //把较大的值赋给当前节点
+                    arr[i] = arr[k];
+                    //i 指向 k，继续循环比较
+                    i = k;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //当for循环结束后，我们已经将以i为父节点的树的最大值，放在了最顶（局部）
+            arr[i] = temp;//将temp的值放到调整后的位置
+        }
+    }
+}
