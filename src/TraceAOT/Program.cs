c1fdc888a1902aec69318a28a286c0b5626e3cdf
Juster.zhu <zhuzhen723723@163.com>
2023-08-30 21:49:27 +0800

    add traceaot

---
+++ src/TraceAOT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace TraceAOT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("MyTraceListeners"));
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Trace.WriteLineIf(i==5, "Trace message.");
+            }
+            Trace.Flush();
+            Console.WriteLine("OK");
+            Console.Read();
+        }
+    }
+}
---
+++ src/TraceDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace TraceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Trace.Write("Trace message.");
+            }
+        }
+    }
+}
