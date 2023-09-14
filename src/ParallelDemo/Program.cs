b6254bc8c398cb38c6d61de434b38d4f0b678e3f
Juster.zhu <zhuzhen723723@163.com>
2023-09-14 22:48:37 +0800

    Parallel demo

---
+++ src/ParallelDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ParallelDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 100);
+
+            Parallel.ForEach(numbers, number =>
+            {
+                Console.WriteLine(number);
+            });
+            Console.Read();
+        }
+    }
+}
