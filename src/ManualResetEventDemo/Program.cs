90c64ec1b28ebd1318fa1cd4bdddf861f20f79b4
Juster.zhu <zhuzhen723723@163.com>
2023-09-19 21:19:11 +0800

    manualresetevent demo

---
+++ src/ManualResetEventDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ManualResetEventDemo
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        private static ManualResetEvent manualEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(ThreadProc);
+            thread1.Name = "Thread1";
+            thread1.Start();
+
+            Thread thread2 = new Thread(ThreadProc);
+            thread2.Name = "Thread2";
+            thread2.Start();
+
+            Console.WriteLine("Waiting for threads to complete.");
+            manualEvent.Set();  // Signal that work can be done.
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("All threads have completed.");
+        }
+
+        static void ThreadProc()
+        {
+            string name = Thread.CurrentThread.Name;
+
+            Console.WriteLine($"{name} waits on ManualResetEvent.");
+            manualEvent.WaitOne();  // Wait for the signal.
+            Console.WriteLine($"{name} receives a signal to proceed.");
+            // Do some work...
+        }
+    }
+
+}
---
+++ src/RxNETDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace RxNETDemo
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            IObservable<StockPrice> stockPrices = GetRealTimeStockPrices();
+
+            IDisposable subscription = stockPrices
+                .Where(price => price.Value > 100)
+                .Subscribe(
+                    price => Console.WriteLine($"High stock price detected: {price.Symbol} at {price.Value}"),
+                    ex => Console.WriteLine($"OnError: {ex.Message}"),
+                    () => Console.WriteLine("OnCompleted"));
+
+            Console.Read( );
+
+            // 在某个时刻，你可能想取消订阅
+            subscription.Dispose();
+        }
+
+        class StockPrice
+        {
+            public string Symbol { get; set; }
+            public double Value { get; set; }
+        }
+
+       static  IObservable<StockPrice> GetRealTimeStockPrices()
+        {
+            // 创建一个 Subject
+            var subject = new Subject<StockPrice>();
+
+            // 在后台线程上生成模拟数据
+            Task.Run(() =>
+            {
+                var random = new Random();
+                while (true)
+                {
+                    var price = new StockPrice
+                    {
+                        Symbol = "MSFT",
+                        Value = 90 + (random.NextDouble() * 20)
+                    };
+
+                    // 向 Subject 发送新的价格
+                    subject.OnNext(price);
+
+                    // 暂停一段时间以模拟实时数据
+                    Thread.Sleep(1000);
+                }
+            });
+
+            // 返回 Observable
+            return subject.AsObservable();
+        }
+
+    }
+
+}
