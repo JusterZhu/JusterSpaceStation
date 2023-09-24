3bd9a314a9de7402989a65acf0d9d34db56b7412
Juster.zhu <zhuzhen723723@163.com>
2023-09-24 20:27:23 +0800

    Attribute demo

---
+++ src/AttributeDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AttributeDemo
+{
+    internal class Program
+    {
+ 
+        public class MyCustomAttribute : Attribute
+        {
+            public string Description { get; }
+
+            public MyCustomAttribute(string description)
+            {
+                Description = description;
+            }
+        }
+
+        [MyCustom("This is a custom attribute")]
+        public class MyClass
+        {
+            // 类的定义
+        }
+
+        static void Main(string[] args)
+        {
+            MyCustomAttribute attribute = (MyCustomAttribute)Attribute.GetCustomAttribute(typeof(MyClass), typeof(MyCustomAttribute));
+
+            if (attribute != null)
+            {
+                Console.WriteLine($"Description: {attribute.Description}");
+            }
+            Console.Read();
+        }
+    }
+}
