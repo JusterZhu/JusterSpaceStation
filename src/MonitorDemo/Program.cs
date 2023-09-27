3fd875aa80c463e0d496c10849c484d49bf3f5d4
Juster.zhu <zhuzhen723723@163.com>
2023-09-27 22:18:27 +0800

    添加monitor demo

---
+++ src/MonitorDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MonitorDemo
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+
+    class Program
+    {
+        private static int counter = 0;
+        private static object lockObject = new object();
+        private static object monitorObject = new object();
+
+        static void Main()
+        {
+            // 使用Monitor进行性能测试
+            Stopwatch monitorStopwatch = new Stopwatch();
+            monitorStopwatch.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (monitorObject)
+                {
+                    counter++;
+                }
+            }
+
+            monitorStopwatch.Stop();
+            Console.WriteLine($"Using Monitor: {monitorStopwatch.ElapsedMilliseconds} ms");
+
+            // 使用lock进行性能测试
+            Stopwatch lockStopwatch = new Stopwatch();
+            lockStopwatch.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Monitor.Enter(lockObject);
+                try
+                {
+                    counter++;
+                }
+                finally
+                {
+                    Monitor.Exit(lockObject);
+                }
+            }
+
+            lockStopwatch.Stop();
+            Console.WriteLine($"Using lock: {lockStopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+
+}
