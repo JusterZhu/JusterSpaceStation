9be959cbbe91907c36350b400532d5fa55d25e43
Juster.zhu <zhuzhen723723@163.com>
2023-11-03 22:07:07 +0800

    添加文件事务demo

---
+++ src/TransactionScopeDemo/FileSystemTransaction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class FileSystemTransaction : IDisposable
+{
+    private IntPtr _transaction;
+
+    [DllImport("ktmw32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    static extern IntPtr CreateTransaction(IntPtr lpTransactionAttributes, IntPtr UOW, int CreateOptions,
+        int IsolationLevel, int IsolationFlags, int Timeout, string Description);
+
+    [DllImport("ktmw32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    static extern bool CommitTransaction(IntPtr TransactionHandle);
+
+    [DllImport("ktmw32.dll", SetLastError = true)]
+    static extern bool RollbackTransaction(IntPtr TransactionHandle);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool CloseHandle(IntPtr hObject);
+
+    public FileSystemTransaction(string description)
+    {
+        _transaction = CreateTransaction(IntPtr.Zero, IntPtr.Zero, 0, 0, 0, 0, description);
+
+        if (_transaction == IntPtr.Zero)
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+    }
+
+    public void Commit()
+    {
+        if (!CommitTransaction(_transaction))
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+    }
+
+    public void Rollback()
+    {
+        if (!RollbackTransaction(_transaction))
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+    }
+
+    public void Dispose()
+    {
+        CloseHandle(_transaction);
+    }
+}
---
+++ src/TransactionScopeDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Transactions;
+
+namespace TransactionScopeDemo
+{
+    internal class Program
+    {
+        // 定义两个要写入文件的路径
+        static string path1 = @"C:\temp\file1.txt";
+        static string path2 = @"C:\temp\file2.txt";
+
+        static void Main(string[] args)
+        {
+            using (var t = new FileSystemTransaction("My Transaction"))
+            {
+                try
+                {
+                    // 执行文件操作
+                    // ...
+                    PerformTransaction();
+                    t.Commit();
+                }
+                catch (Exception)
+                {
+                    t.Rollback();
+                }
+                finally
+                {
+                    // 检查文件是否存在
+                    Console.WriteLine($"Is {path1} exists? {File.Exists(path1)}");
+                    Console.WriteLine($"Is {path2} exists? {File.Exists(path2)}");
+                }
+            }
+
+            Console.Read();
+        }
+
+        public static void PerformTransaction()
+        {
+            // 写入第一个文件
+            File.WriteAllText(path1, "Hello, world!");
+            // 写入第二个文件
+            File.WriteAllText(path2, "Welcome to C#!");
+            throw new Exception();
+        }
+    }
+}
