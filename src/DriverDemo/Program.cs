e883203b63bb2357107515f12aca4b672fc25740
Juster Zhu <zhuzhen723723@163.com>
2023-12-06 22:26:29 +0800

    Update Program.cs

--- src/DriverDemo/Program.cs
+++ src/DriverDemo/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace DriverDemo
+﻿using GeneralUpdate.Core.Driver;
+
+namespace DriverDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var driverInformation = new DriverInformation.Builder()
+                .SetDriverNames(null)
+                .SetDriverNames(null)
+                .SetOutPutDirectory(null)
+                .Build();
+
+            var driverProcessor = new DriverProcessor();
+            driverProcessor.AddCommand(new BackupDriverCommand(driverInformation));
+            driverProcessor.ProcessCommands();
         }
     }
 }
