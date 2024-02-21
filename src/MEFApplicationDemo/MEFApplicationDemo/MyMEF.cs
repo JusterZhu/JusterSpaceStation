43729e7089263fb07a3af57304e7061b0d233cc1
Juster Zhu <zhuzhen723723@163.com>
2024-02-21 23:14:05 +0800

    添加MEF Demo

---
+++ src/MEFApplicationDemo/MEFApplicationDemo/MyMEF.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEFApplicationDemo;
+
+public class MyMEF
+{
+    private void Init()
+    {
+        var catalog = new AggregateCatalog();
+        catalog.Catalogs.Add(new AssemblyCatalog(typeof(MyMEF).Assembly));
+        var container = new CompositionContainer(catalog);
+    }
+}
