dfcd6b9b15329e2db4f846cf089904722a57eb8a
Juster Zhu <zhuzhen723723@163.com>
2024-03-10 20:16:57 +0800

    WixSharpDemo

--- src/SemanticKernelDemo/MyAnalyzer.cs
+++ src/SemanticKernelDemo/MyAnalyzer.cs
@@ -15,22 +15,22 @@ namespace SemanticKernelDemo
         public void Init() 
         {
             //Uri endpoint, TokenCredential credential
-            var uri = new Uri("Your Endpoint");
-            var credentials = new TokenCredential(); //new ApiKeyServiceClientCredentials("Your Subscription Key");
-            var client = new TextAnalyticsClient(uri, credentials);
+            //var uri = new Uri("Your Endpoint");
+            //var credentials = new TokenCredential(); //new ApiKeyServiceClientCredentials("Your Subscription Key");
+            //var client = new TextAnalyticsClient(uri, credentials);
 
-            var input = new MultiLanguageBatchInput(
-                new List<MultiLanguageInput>()
-                {
-                    new MultiLanguageInput("en", "1", "The hotel was dark and uninviting.")
-                });
+            //var input = new MultiLanguageBatchInput(
+            //    new List<MultiLanguageInput>()
+            //    {
+            //        new MultiLanguageInput("en", "1", "The hotel was dark and uninviting.")
+            //    });
 
-            var result = client.AnalyzeSentimentAsync(false, input).Result;
+            //var result = client.AnalyzeSentimentAsync(false, input).Result;
 
-            foreach (var document in result.Documents)
-            {
-                Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
-            }
+            //foreach (var document in result.GetRawResponse())
+            //{
+            //    Console.WriteLine($"Document ID: {document.Id} , Sentiment Score: {document.Score:0.00}");
+            //}
         }
     }
 }
---
+++ src/WixSharpSetupDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using WixSharp;
+using WixSharp.UI.WPF;
+
+namespace WixSharpSetupDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var project = new ManagedProject("MyProduct",
+                              new Dir(@"%ProgramFiles%\My Company\My Product",
+                                  new File("Program.cs")));
+
+            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+
+            // project.ManagedUI = ManagedUI.DefaultWpf; // all stock UI dialogs
+
+            //custom set of UI WPF dialogs
+            project.ManagedUI = new ManagedUI();
+
+            project.ManagedUI.InstallDialogs.Add<WixSharpSetupDemo.WelcomeDialog>()
+                                            .Add<WixSharpSetupDemo.LicenceDialog>()
+                                            .Add<WixSharpSetupDemo.FeaturesDialog>()
+                                            .Add<WixSharpSetupDemo.InstallDirDialog>()
+                                            .Add<WixSharpSetupDemo.ProgressDialog>()
+                                            .Add<WixSharpSetupDemo.ExitDialog>();
+
+            project.ManagedUI.ModifyDialogs.Add<WixSharpSetupDemo.MaintenanceTypeDialog>()
+                                           .Add<WixSharpSetupDemo.FeaturesDialog>()
+                                           .Add<WixSharpSetupDemo.ProgressDialog>()
+                                           .Add<WixSharpSetupDemo.ExitDialog>();
+
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
+
+            project.BuildMsi();
+        }
+    }
+}
