b61518b79d78529b8c6e26e86a8f6a280177c1a0
Juster Zhu <zhuzhen723723@163.com>
2024-03-29 00:03:37 +0800

    右侧显示treeview

---
+++ src/TreeViewRightExpanderDemo/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewRightExpanderDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Node> FirstLevelNodes { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FirstLevelNodes = new ObservableCollection<Node>
+    {
+        new Node { NodeName = "Node1", SecondLevelNodes = new ObservableCollection<Node>
+            {
+                new Node { NodeName = "Child1" },
+                new Node { NodeName = "Child2" }
+            }
+        },
+        new Node { NodeName = "Node2", SecondLevelNodes = new ObservableCollection<Node>
+            {
+                new Node { NodeName = "Child3" },
+                new Node { NodeName = "Child4" }
+            }
+        }
+    };
+
+            DataContext = this;
+        }
+
+        public class Node
+        {
+            public string NodeName { get; set; }
+            public ObservableCollection<Node> SecondLevelNodes { get; set; }
+        }
+
+        public class ViewModel
+        {
+            public ObservableCollection<Node> FirstLevelNodes { get; set; }
+        }
+    }
+}
