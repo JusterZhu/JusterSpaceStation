646dec27a0f80b7354b84c48c3046d83704c584e
Juster Zhu <zhuzhen723723@163.com>
2024-04-14 22:49:09 +0800

    Update MainWindowViewModel.cs

--- src/LiveChart2Demo/ViewModels/MainWindowViewModel.cs
+++ src/LiveChart2Demo/ViewModels/MainWindowViewModel.cs
@@ -5,14 +5,13 @@ namespace LiveChart2Demo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public ISeries[] Series { get; set; }
-                    = new ISeries[]
-                    {
+        public ISeries[] Series { get; set; } =
+        {
                 new LineSeries<double>
                 {
                     Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
                     Fill = null
                 }
-                    };
+        };
     }
 }
