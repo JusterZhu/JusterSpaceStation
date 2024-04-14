b80ce19402f434726264fdcdf11ef9362a536a71
Juster Zhu <zhuzhen723723@163.com>
2024-04-14 22:45:25 +0800

    添加live chart demo

---
+++ src/LiveChart2Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+
+namespace LiveChart2Demo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ISeries[] Series { get; set; }
+                    = new ISeries[]
+                    {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+                    };
+    }
+}
---
+++ src/LiveChart2Demo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LiveChart2Demo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
