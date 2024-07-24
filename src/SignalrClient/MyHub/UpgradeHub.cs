c6de189986bb815d5baaed02be42876e5028d41b
justerzhu <zhuzhen723723@outlook.com>
2024-07-24 23:37:57 +0800

    feature: 添加signal r demo

---
+++ src/SignalrClient/MyHub/UpgradeHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalrClient.MyHub;
+
+public class UpgradeHub
+{
+    private HubConnection _connection;
+
+    public UpgradeHub()
+    {
+        _connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5000/chatHub")
+            .Build();
+
+        _connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            Console.WriteLine($"{user}: {message}");
+        });
+    }
+
+    public async Task StartAsync()
+    {
+        await _connection.StartAsync();
+        Console.WriteLine("Connection started");
+    }
+
+    public async Task SendMessageAsync(string user, string message)
+    {
+        await _connection.InvokeAsync("SendMessage", user, message);
+    }
+}
---
+++ src/SignalrClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using SignalrClient.MyHub;
+
+namespace SignalrClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        UpgradeHub upgradeHub = new UpgradeHub();
+        await upgradeHub.StartAsync();
+        
+        while (true)
+        {
+           var content = Console.ReadLine();
+           if (content == "exit") break;
+        }
+    }
+}
