b877fed029f933c6263ae072b3a5303c01272bdd
Juster.zhu <zhuzhen723723@163.com>
2024-08-14 20:48:29 +0800

    SFTP Demo

---
+++ src/FindFilesDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FindFilesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDirectory = @"D:\test\a";
+            string targetDirectory = @"D:\test\b";
+            string resultDir = @"D:\test\result";
+
+            CopyMatchingFiles(sourceDirectory, targetDirectory, resultDir);
+            Console.Read();
+        }
+
+        public static void CopyMatchingFiles(string appPath, string patchPath, string destinationPath)
+        {
+            foreach (string filePath in Directory.GetFiles(patchPath, "*.*", SearchOption.AllDirectories))
+            {
+                string correspondingPathInA = filePath.Replace(patchPath, appPath);
+
+                if (File.Exists(correspondingPathInA))
+                {
+                    string correspondingPathInC = filePath.Replace(patchPath, destinationPath);
+
+                    string directoryName = Path.GetDirectoryName(correspondingPathInC);
+                    if (!Directory.Exists(directoryName))
+                    {
+                        Directory.CreateDirectory(directoryName);
+                    }
+                    File.Copy(correspondingPathInA, correspondingPathInC, true);
+                }
+            }
+        }
+    }
+}
---
+++ src/SFTDemo2/SFTPManage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinSCP;
+
+namespace SFTDemo2
+{
+    internal class SFTPManage
+    {
+        internal void Test() 
+        {
+            // 设置会话选项
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Sftp,
+                HostName = "192.168.1.254", // 服务器地址
+                UserName = "zhuzhen", // 用户名
+                Password = "123456789", // 密码
+                SshHostKeyFingerprint = "ssh-rsa 2048 tX+GJ5ArF+E6UpBs15dQcKQTrxeXAaom2ApxnSJpLfo"
+            };
+
+            using (Session session = new Session())
+            {
+                // 连接到服务器
+                session.Open(sessionOptions);
+
+                // 指定远程目录路径
+                string remotePath = "C:\\SftpRoot";
+
+                // 获取指定目录下的文件列表
+                RemoteDirectoryInfo directoryInfo = session.ListDirectory(remotePath);
+
+                // 打印出所有文件名
+                foreach (RemoteFileInfo fileInfo in directoryInfo.Files)
+                {
+                    Console.WriteLine("Name: {0}", fileInfo.Name);
+                }
+            }
+        }
+    }
+}
