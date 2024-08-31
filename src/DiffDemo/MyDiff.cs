b8e296688cdb40b2eb11c13abf25ad65ba1e481b
justerzhu <zhuzhen723723@outlook.com>
2024-09-01 00:33:33 +0800

    feature: 差分

---
+++ src/DiffDemo/MyDiff.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography;
+
+namespace DiffDemo;
+
+public class MyDiff
+{
+    private static readonly List<string> DefaultBlackFileFormats = new List<string>
+    {
+        ".patch",
+        ".7z",
+        ".zip",
+        ".rar",
+        ".tar",
+        ".json"
+    };
+
+    private static readonly List<string> DefaultBlackFiles = new List<string> { "Newtonsoft.Json.dll" };
+
+    public static void CompareDirectories(string dirA, string dirB, List<string> uniqueToA, List<string> uniqueToB, List<string> differentFiles)
+    {
+        var filesA = GetRelativeFilePaths(dirA, dirA).Where(f => !IsBlacklisted(f)).ToList();
+        var filesB = GetRelativeFilePaths(dirB, dirB).Where(f => !IsBlacklisted(f)).ToList();
+
+        uniqueToA.AddRange(filesA.Except(filesB).Select(f => Path.Combine(dirA, f)));
+        uniqueToB.AddRange(filesB.Except(filesA).Select(f => Path.Combine(dirB, f)));
+
+        var commonFiles = filesA.Intersect(filesB);
+
+        foreach (var file in commonFiles)
+        {
+            var fileA = Path.Combine(dirA, file);
+            var fileB = Path.Combine(dirB, file);
+
+            if (!FilesAreEqual(fileA, fileB))
+            {
+                differentFiles.Add(file);
+            }
+        }
+    }
+
+    static IEnumerable<string> GetRelativeFilePaths(string rootDir, string currentDir)
+    {
+        foreach (var file in Directory.GetFiles(currentDir))
+        {
+            yield return Path.GetRelativePath(rootDir, file);
+        }
+
+        foreach (var dir in Directory.GetDirectories(currentDir))
+        {
+            foreach (var file in GetRelativeFilePaths(rootDir, dir))
+            {
+                yield return file;
+            }
+        }
+    }
+
+    static bool IsBlacklisted(string relativeFilePath)
+    {
+        var fileName = Path.GetFileName(relativeFilePath);
+        var fileExtension = Path.GetExtension(relativeFilePath);
+
+        return DefaultBlackFiles.Contains(fileName) || DefaultBlackFileFormats.Contains(fileExtension);
+    }
+
+    static bool FilesAreEqual(string fileA, string fileB)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var hashA = sha256.ComputeHash(File.ReadAllBytes(fileA));
+            var hashB = sha256.ComputeHash(File.ReadAllBytes(fileB));
+
+            return hashA.SequenceEqual(hashB);
+        }
+    }
+}
---
+++ src/DiffDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DiffDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dirA = @"D:\Program Files\Tencent\QQNTbakA";
+        string dirB = @"D:\Program Files\Tencent\QQNTbakB";
+
+        var uniqueToA = new List<string>();
+        var uniqueToB = new List<string>();
+        var differentFiles = new List<string>();
+
+        MyDiff.CompareDirectories(dirA, dirB, uniqueToA, uniqueToB, differentFiles);
+
+        Console.WriteLine("Files unique to A:");
+        uniqueToA.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nFiles unique to B:");
+        uniqueToB.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nDifferent files:");
+        differentFiles.ForEach(Console.WriteLine);
+        
+        Console.Read();
+    }
+}
