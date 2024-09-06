5c5f01d86b326c420bc30852384c2fada262d2cd
justerzhu <zhuzhen723723@outlook.com>
2024-09-06 22:16:30 +0800

    feature: 动态添加view

--- src/PrismTableDemo/MainWindow.xaml.cs
+++ src/PrismTableDemo/MainWindow.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using PrismTableDemo.Views;
 
 namespace PrismTableDemo;
@@ -42,5 +32,10 @@ public partial class MainWindow : Window
     private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
     {
         _regionManager.RegisterViewWithRegion("TabRegion", typeof(ViewA));
+    }
+
+    private void BtnShowViewC_OnClick(object sender, RoutedEventArgs e)
+    {
+        _regionManager.RegisterViewWithRegion("TabRegion", typeof(ViewC));
     }
 }
