a51b02a6eaea363bcef168b13324dc1ac17e0893
justerzhu <zhuzhen723723@outlook.com>
2024-09-14 23:34:20 +0800

    structlayout demo

---
+++ src/StructLayoutDemo/ExplicitStruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StructLayoutDemo;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct ExplicitStruct
+{
+    [FieldOffset(0)]
+    public int Field1;
+    [FieldOffset(4)]
+    public short Field2;
+    [FieldOffset(6)]
+    public byte Field3;
+}
---
+++ src/StructLayoutDemo/OptimizedStruct.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StructLayoutDemo;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct OptimizedStruct
+{
+    public int Field1;
+    public short Field2;
+    public byte Field3; // 放在最后以减少填充
+}
