a6bb3cafd0af4da6c230febbc0c8d27a0b19a0ed
justerzhu <zhuzhen723723@outlook.com>
2024-09-15 23:32:14 +0800

    切换viewmodel拿值

--- src/PrismTableDemo/MyViewModel.cs
+++ src/PrismTableDemo/MyViewModel.cs
@@ -1,6 +1,26 @@
 ﻿namespace PrismTableDemo;
 
+public class ViewModelStore
+{
+    private Dictionary<string, object> _viewModels = new Dictionary<string, object>();
+
+    public T GetOrCreateViewModel<T>(string key) where T : new()
+    {
+        if (!_viewModels.ContainsKey(key))
+        {
+            _viewModels[key] = new T();
+        }
+        return (T)_viewModels[key];
+    }
+}
+
 public class MyViewModel
 {
+    ViewModelStore viewModelStore = new ViewModelStore();
     
+    public MyViewModel()
+    {
+        // 在加载View时
+        var viewModel = viewModelStore.GetOrCreateViewModel<MyViewModel>("View1");
+    }
 }
